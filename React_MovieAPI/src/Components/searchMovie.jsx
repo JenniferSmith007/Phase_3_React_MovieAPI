e2d85511734e44d8d379c9f4f0b02c0719299ec2
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-09-08 13:30:30 -0400

    axios api call status 200

--- React_MovieAPI/src/Components/searchMovie.jsx
+++ React_MovieAPI/src/Components/searchMovie.jsx
@@ -1,8 +1,21 @@
 import { Header } from "./header";
 import { Link } from "react-router-dom";
+import movieapi from "./movieapi";
+import { useEffect } from "react";
 
 export const Searchmovie = () => {
+useEffect(() => {
+    let apiKey = '896bcbd1'
+    let movieex = 'naruto'
+    const getMovie = async () => {
+    const res = await movieapi.get(`?s=${movieex}&plot=short&apikey=${apiKey}`).catch((error) => {
+            console.log('error', error)
+        })
+      console.log(res)  
+    }
+    getMovie()
 
+}, [])
 
 
 
