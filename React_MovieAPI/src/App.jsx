2e99c81e7c789104aa5722af0a43bcb249293e2b
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-09-12 18:55:35 -0400

    testing

--- React_MovieAPI/src/App.jsx
+++ React_MovieAPI/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  Searchmovie  from "./Components/SearchMovie";
 import  FavoriteMovie  from "./Components/FavoriteMovie";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Searchmovie />} />
+        <Route path="/" element={<Searchmovie/>} />
         <Route path="/favoritemovies" element={<FavoriteMovie />} />
       </Routes>
     </Router>
